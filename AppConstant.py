0cfa5f753c20b41fa5763ebb0b95e85c65c0a372
admin <iddqd2d@gmail.com>
2025-09-11 15:47:33 +0300

    new admins

--- AppConstant.py
+++ AppConstant.py
@@ -2,7 +2,9 @@
 class AppConstant:
     VALHALLA_DIR = "/root/valhalla_result"
     VALHALLA_THREADS = 1
-    PBF_FILE_URL = "https://download.geofabrik.de/north-america/canada/new-brunswick-latest.osm.pbf"
+    REGION_PBF_FILE_URL = "https://download.geofabrik.de/north-america-latest.osm.pbf"
+    ADMINS_PBF_FILE_URL = "https://planet.openstreetmap.org/pbf/planet-latest.osm.pb"
+
 
     CHECK_EXPIRED_TIME = False
     EXPIRED_TIME = 604800  # 1 week
@@ -12,7 +14,8 @@ class AppConstant:
     TEMP_TILES_TAR_FILE = VALHALLA_DIR + "/temp_valhalla_tiles.tar"
     CONFIG_FILE = VALHALLA_DIR + "/conf/valhalla.json"
     LOG_FILE_PATH = VALHALLA_DIR + "/valhalla_update.log"
-    PBF_FILE_ABSOLUTE_PATH = VALHALLA_DIR + "/tiles.pbf"
+    REGION_PBF_FILE_ABSOLUTE_PATH = VALHALLA_DIR + "/tiles.pbf"
+    ADMINS_PBF_FILE_ABSOLUTE_PATH = VALHALLA_DIR + "/admins.pbf"
 
 
 
