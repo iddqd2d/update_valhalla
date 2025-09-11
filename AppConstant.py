79f45fb8bf1b9be3ef87488594a06343887c58be
admin <iddqd2d@gmail.com>
2025-09-11 16:47:10 +0300

    new admins

--- AppConstant.py
+++ AppConstant.py
@@ -3,7 +3,7 @@ class AppConstant:
     VALHALLA_DIR = "/root/valhalla_result"
     VALHALLA_THREADS = 1
     REGION_PBF_FILE_URL = "https://download.geofabrik.de/north-america-latest.osm.pbf"
-    ADMINS_PBF_FILE_URL = "https://planet.openstreetmap.org/pbf/planet-latest.osm.pb"
+    ADMINS_PBF_FILE_URL = "https://planet.openstreetmap.org/pbf/planet-latest.osm.pbf"
 
 
     CHECK_EXPIRED_TIME = False
